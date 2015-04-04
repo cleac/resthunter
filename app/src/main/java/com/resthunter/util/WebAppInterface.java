dab6880f5bd27ef2c64dcdf37c5fcb02afbfc24b
Olexandr Kovalchuk <me.olexandr.kovalchuk@gmail.com>
2015-04-05 01:16:29 +0300

    Webview Activity basics

---
+++ app/src/main/java/com/resthunter/util/WebAppInterface.java
@@ -0,0 +1,25 @@
+package com.resthunter.util;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class WebAppInterface {
+  Context mContext;
+
+  /** Instantiate the interface and set the context */
+  public WebAppInterface(Context c) {
+    mContext = c;
+  }
+
+  /** Show a toast from the web page */
+  @JavascriptInterface
+  public void showToast(String toast) {
+    Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+  }
+
+  @JavascriptInterface
+  public void bookTable(String id) {
+    Toast.makeText(mContext,  "Table " + id + " booked", Toast.LENGTH_SHORT).show();
+  }
+}
