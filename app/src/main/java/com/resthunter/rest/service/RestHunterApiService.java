32041377223e16ca540176c6cc75a3df030486d5
Alex Nesterenko <nestorf250@gmail.com>
2015-04-04 18:49:25 +0300

    Added possibility of getting ordered lists

--- app/src/main/java/com/resthunter/rest/service/RestHunterApiService.java
+++ app/src/main/java/com/resthunter/rest/service/RestHunterApiService.java
@@ -12,6 +12,7 @@ import java.util.concurrent.Callable;
 import javax.security.auth.callback.Callback;
 
 import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * Created by cleac on 04.04.15.
@@ -32,4 +33,20 @@ public interface RestHunterApiService {
 
     @GET("/api/restaurant?format=json")
     public void getRestaurantList(retrofit.Callback<ArrayList<Restaurant>> callback);
+
+
+    @GET("/api/dish?format=json&ordering={field}")
+    public void getDishList(@Path("field") String field,retrofit.Callback<ArrayList<Dish>> callback);
+
+    @GET("/api/ingredient?format=json&ordering={field}")
+    public void getIngredientList(@Path("field") String field,retrofit.Callback<ArrayList<Ingredient>> callback);
+
+    @GET("/api/menu?format=json&ordering={field}")
+    public void getMenu(@Path("field") String field,retrofit.Callback<ArrayList<MenuEntry>> callback);
+
+    @GET("/api/category?forman=json&ordering={field}")
+    public void getCategoryList(@Path("field") String field,retrofit.Callback<ArrayList<Category>> callback);
+
+    @GET("/api/restaurant?format=json&ordering={field}")
+    public void getRestaurantList(@Path("field") String field,retrofit.Callback<ArrayList<Restaurant>> callback);
 }
